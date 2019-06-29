a2c4c178c505760907356dea5c3bfa8b56b82627
tevin davis <aod.td1105@gmail.com>
2019-06-29 17:31:27 -0400

    Completed challenges for call back file

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,24 +27,40 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
+getLength(items, console.log);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr);
 }
+last(items[items.length-1], console.log)
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+sumNums(3, 4, console.log)
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+multiplyNums(3, 15, console.log)
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if(list.includes(item)) {
+    cb(true);
+  }else {
+    cb(false);
+  }
 }
+
+const cars = ['Mustang', 'Corvette', 'Challenger', 'Viper'];
+contains('Camaro', cars, console.log)
 
 /* STRETCH PROBLEM */
 
